f57a462345541cb93f925a5d6c999a10eaee61b7
Jure Bajic <jbajic@users.noreply.github.com>
2021-10-07 09:02:59 +0200

    Add support for temporal types (#21)

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -20,34 +20,23 @@ namespace mg_memory {
 /// Unique pointers with custom deleters for automatic memory management of
 /// mg_values.
 
-template <class T>
-inline void CustomDelete(T *);
+template <class T> inline void CustomDelete(T *);
 
-template <>
-inline void CustomDelete(mg_session *session) {
+template <> inline void CustomDelete(mg_session *session) {
   mg_session_destroy(session);
 }
 
-template <>
-inline void CustomDelete(mg_session_params *session_params) {
+template <> inline void CustomDelete(mg_session_params *session_params) {
   mg_session_params_destroy(session_params);
 }
 
-template <>
-inline void CustomDelete(mg_list *list) {
-  mg_list_destroy(list);
-}
+template <> inline void CustomDelete(mg_list *list) { mg_list_destroy(list); }
 
-template <>
-inline void CustomDelete(mg_map *map) {
-  mg_map_destroy(map);
-}
+template <> inline void CustomDelete(mg_map *map) { mg_map_destroy(map); }
 
-template <class T>
-using CustomUniquePtr = std::unique_ptr<T, void (*)(T *)>;
+template <class T> using CustomUniquePtr = std::unique_ptr<T, void (*)(T *)>;
 
-template <class T>
-CustomUniquePtr<T> MakeCustomUnique(T *ptr) {
+template <class T> CustomUniquePtr<T> MakeCustomUnique(T *ptr) {
   return CustomUniquePtr<T>(ptr, CustomDelete<T>);
 }
 
@@ -56,28 +45,27 @@ using MgSessionParamsPtr = CustomUniquePtr<mg_session_params>;
 using MgListPtr = CustomUniquePtr<mg_list>;
 using MgMapPtr = CustomUniquePtr<mg_map>;
 
-}  // namespace mg_memory
+} // namespace mg_memory
 
 namespace utils {
 
 class ClientFatalException : public std::exception {
- public:
+public:
   ClientFatalException(std::string what) : what_(std::move(what)) {}
   const char *what() const noexcept override { return what_.c_str(); }
 
- private:
+private:
   std::string what_;
 };
 
 class ClientQueryException : public std::exception {
- public:
+public:
   ClientQueryException(std::string what) : what_(std::move(what)) {}
   const char *what() const noexcept override { return what_.c_str(); }
 
- private:
+private:
   std::string what_;
 };
-
 
 bool EnsureDir(const fs::path &dir) noexcept;
 
@@ -86,7 +74,7 @@ fs::path GetUserHomeDir();
 /**
  * return string with all uppercased characters (locale independent).
  */
-std::string ToUpperCase(std::string s); 
+std::string ToUpperCase(std::string s);
 
 /**
  * removes whitespace characters from the start and from the end of a string.
@@ -101,8 +89,8 @@ std::string Trim(const std::string &s);
  * replaces all occurences of <match> in <src> with <replacement>.
  */
 // todo: this could be implemented much more efficiently.
-std::string Replace(std::string src, const std::string &match, const std::string &replacement);
-
+std::string Replace(std::string src, const std::string &match,
+                    const std::string &replacement);
 
 /// escapes all whitespace and quotation characters to produce a string
 /// which can be used as a string literal.
@@ -119,7 +107,8 @@ std::string Escape(const std::string &src);
  *  streams the item to the stream.
  */
 template <typename tstream, typename titerable>
-inline void PrintIterable(tstream &stream, const titerable &iterable, const std::string &delim = ", ") {
+inline void PrintIterable(tstream &stream, const titerable &iterable,
+                          const std::string &delim = ", ") {
   bool first = true;
   for (const auto &item : iterable) {
     if (first)
@@ -144,9 +133,17 @@ void PrintValue(std::ostream &os, const mg_unbound_relationship *rel);
 
 void PrintValue(std::ostream &os, const mg_path *path);
 
+void PrintValue(std::ostream &os, const mg_date *date);
+
+void PrintValue(std::ostream &os, const mg_local_time *local_time);
+
+void PrintValue(std::ostream &os, const mg_local_date_time *local_date_time);
+
+void PrintValue(std::ostream &os, const mg_duration *duration);
+
 void PrintValue(std::ostream &os, const mg_value *value);
 
-}  // namespace utils
+} // namespace utils
 
 // Unfinished query text from previous input.
 // e.g. Previous input was MATCH(n) RETURN n; MATCH
@@ -161,7 +158,8 @@ void PrintHelp();
 
 void PrintDocs();
 
-void EchoFailure(const std::string &failure_msg, const std::string &explanation);
+void EchoFailure(const std::string &failure_msg,
+                 const std::string &explanation);
 
 void EchoInfo(const std::string &message);
 
@@ -178,7 +176,8 @@ std::optional<std::string> GetLine();
 /// @param escaped if set, next character should be escaped.
 /// @return pair of string and bool. string is parsed line and bool marks
 /// if query finished(Query finishes with ';') with this line.
-std::pair<std::string, bool> ParseLine(const std::string &line, char *quote, bool *escaped);
+std::pair<std::string, bool> ParseLine(const std::string &line, char *quote,
+                                       bool *escaped);
 
 /// Helper function that reads a line from the
 /// standard input using the 'readline' lib.
@@ -188,7 +187,7 @@ std::pair<std::string, bool> ParseLine(const std::string &line, char *quote, boo
 std::optional<std::string> ReadLine(Replxx *replxx_instance,
                                     const std::string &prompt);
 
-}  // namespace console
+} // namespace console
 
 namespace query {
 
@@ -202,21 +201,21 @@ std::optional<std::string> GetQuery(Replxx *replxx_instance);
 
 QueryData ExecuteQuery(mg_session *session, const std::string &query);
 
-}  // namespace query
+} // namespace query
 
 namespace format {
 
 struct CsvOptions {
   CsvOptions(std::string delim, std::string escape, const bool dquote)
-    : delimiter(std::move(delim)), escapechar(std::move(escape)), doublequote(dquote) {}
+      : delimiter(std::move(delim)), escapechar(std::move(escape)),
+        doublequote(dquote) {}
 
   bool ValidateDoubleQuote() {
-  if (!doublequote && escapechar.size() != 1) {
-    return false;
+    if (!doublequote && escapechar.size() != 1) {
+      return false;
+    }
+    return true;
   }
-  return true;
-}
-
 
   std::string delimiter;
   std::string escapechar;
@@ -225,14 +224,15 @@ struct CsvOptions {
 
 struct OutputOptions {
   OutputOptions(std::string out_format, const bool fit_to_scr)
-    : output_format(std::move(out_format)), fit_to_screen(fit_to_scr) {}
+      : output_format(std::move(out_format)), fit_to_screen(fit_to_scr) {}
 
   std::string output_format;
   bool fit_to_screen;
 };
 
-void PrintHeaderTabular(const std::vector<std::string> &data, int total_width, int column_width,
-                        int num_columns, bool all_columns_fit, int margin);
+void PrintHeaderTabular(const std::vector<std::string> &data, int total_width,
+                        int column_width, int num_columns, bool all_columns_fit,
+                        int margin);
 
 /// Helper function for determining maximum length of data.
 /// @param data List of mg_values representing row.
@@ -243,17 +243,19 @@ uint64_t GetMaxColumnWidth(const mg_memory::MgListPtr &data, int margin);
 
 uint64_t GetMaxColumnWidth(const std::vector<std::string> &data, int margin);
 
-
-void PrintRowTabular(const mg_memory::MgListPtr &data, int total_width, int column_width, int num_columns,
-                     bool all_columns_fit, int margin);
+void PrintRowTabular(const mg_memory::MgListPtr &data, int total_width,
+                     int column_width, int num_columns, bool all_columns_fit,
+                     int margin);
 
 void PrintTabular(const std::vector<std::string> &header,
                   const std::vector<mg_memory::MgListPtr> &records,
                   const bool fit_to_screen);
 
-std::vector<std::string> FormatCsvFields(const mg_memory::MgListPtr &fields, const CsvOptions &csv_opts);
+std::vector<std::string> FormatCsvFields(const mg_memory::MgListPtr &fields,
+                                         const CsvOptions &csv_opts);
 
-std::vector<std::string> FormatCsvHeader(const std::vector<std::string> &fields, const CsvOptions &csv_opts);
+std::vector<std::string> FormatCsvHeader(const std::vector<std::string> &fields,
+                                         const CsvOptions &csv_opts);
 
 void PrintCsv(const std::vector<std::string> &header,
               const std::vector<mg_memory::MgListPtr> &records,
@@ -261,8 +263,7 @@ void PrintCsv(const std::vector<std::string> &header,
 
 void Output(const std::vector<std::string> &header,
             const std::vector<mg_memory::MgListPtr> &records,
-            const OutputOptions &out_opts,
-            const CsvOptions &csv_opts);
-}  // namespace format
+            const OutputOptions &out_opts, const CsvOptions &csv_opts);
+} // namespace format
 
 Replxx *InitAndSetupReplxx();
