b230269f003c00155384017403071aab3980f383
Marko Budiselic <mbudiselicbuda@gmail.com>
2023-05-20 17:41:12 +0000

    WIP

--- build-generic-linux.sh
+++ build-generic-linux.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
-
-set -Eeuo pipefail
+# set -Eeuo pipefail
+# TODO(gitbuda): Fails if underlying docker_exec fails -> FIX -> but maybe this is optimal :thinking:
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$SCRIPT_DIR"
-
 docker_name="mgconsole_build_generic_linux"
+toolchain_url="https://s3-eu-west-1.amazonaws.com/deps.memgraph.io/toolchain-v4/toolchain-v4-binaries-centos-7-x86_64.tar.gz"
+toolchain_tar_gz="$(basename $toolchain_url)"
+memgraph_repo="https://github.com/memgraph/memgraph.git"
+setup_cmd="cd /memgraph/environment/os && \
+  ./centos-7.sh install TOOLCHAIN_RUN_DEPS && \
+  ./centos-7.sh install MEMGRAPH_BUILD_DEPS"
+mgconsole_build_cmd="source /opt/toolchain-v4/activate && \
+  mkdir -p /mgconsole/build && cd /mgconsole/build && \
+  cmake -DCMAKE_BUILD_TYPE=Release -DMGCONSOLE_STATIC_SSL=ON -DOPENSSL_ROOT_DIR=/usr/lib64 .. && make -j"
+
+# TODO(gitbuda): Check for Docker
+if [ ! "$(docker info)" ]; then
+  echo "ERROR: Docker is required"
+  exit 1
+fi
+
 if [ ! "$(docker ps -q -f name=$docker_name)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=$docker_name)" ]; then
         echo "Cleanup of the old exited mgconsole build container..."
@@ -17,9 +32,15 @@ echo "The mgconsole build container is active!"
 
 docker_exec () {
   cmd="$1"
-  docker exec "$docker_name" bash -c "$cmd"
+  docker exec -it "$docker_name" bash -c "$cmd"
 }
 
 docker_exec "mkdir -p /mgconsole"
-docker cp "$PROJECT_ROOT/." "$docker_name:/mgconsole/"
-docker_exec "ls /mgconsole"
+docker cp -q "$PROJECT_ROOT/." "$docker_name:/mgconsole/"
+docker_exec "rm -rf /mgconsole/build"
+docker_exec "yum install -y wget git"
+docker_exec "[ ! -f /$toolchain_tar_gz ] && wget -O /$toolchain_tar_gz $toolchain_url"
+docker_exec "[ ! -d /opt/toolchain-v4/ ] && tar -xzf /$toolchain_tar_gz -C /opt"
+docker_exec "[ ! -d /memgraph/ ] && git clone $memgraph_repo"
+# docker_exec "$setup_cmd"
+docker_exec "$mgconsole_build_cmd"
