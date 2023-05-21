ed9dab7db0a59fb8141a632cc253656f78ee0b7c
Marko Budiselic <mbudiselicbuda@gmail.com>
2023-05-21 08:53:13 +0000

    Some polishments

--- build-generic-linux.sh
+++ build-generic-linux.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# set -Eeuo pipefail
-# TODO(gitbuda): Fails if underlying docker_exec fails -> FIX -> but maybe this is optimal :thinking:
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$SCRIPT_DIR"
+
 docker_name="mgconsole_build_generic_linux"
 toolchain_url="https://s3-eu-west-1.amazonaws.com/deps.memgraph.io/toolchain-v4/toolchain-v4-binaries-centos-7-x86_64.tar.gz"
 toolchain_tar_gz="$(basename $toolchain_url)"
@@ -13,9 +11,8 @@ setup_cmd="cd /memgraph/environment/os && \
   ./centos-7.sh install MEMGRAPH_BUILD_DEPS"
 mgconsole_build_cmd="source /opt/toolchain-v4/activate && \
   mkdir -p /mgconsole/build && cd /mgconsole/build && \
-  cmake -DCMAKE_BUILD_TYPE=Release -DMGCONSOLE_STATIC_SSL=ON -DOPENSSL_ROOT_DIR=/usr/lib64 .. && make -j"
+  cmake -DCMAKE_BUILD_TYPE=Release .. && make -j"
 
-# TODO(gitbuda): Check for Docker
 if [ ! "$(docker info)" ]; then
   echo "ERROR: Docker is required"
   exit 1
@@ -26,7 +23,7 @@ if [ ! "$(docker ps -q -f name=$docker_name)" ]; then
         echo "Cleanup of the old exited mgconsole build container..."
         docker rm $docker_name
     fi
-    docker run -d --name "$docker_name" centos:7 sleep infinity
+    docker run -d --network host --name "$docker_name" centos:7 sleep infinity
 fi
 echo "The mgconsole build container is active!"
 
@@ -37,10 +34,12 @@ docker_exec () {
 
 docker_exec "mkdir -p /mgconsole"
 docker cp -q "$PROJECT_ROOT/." "$docker_name:/mgconsole/"
-docker_exec "rm -rf /mgconsole/build"
+docker_exec "rm -rf /mgconsole/build/*"
 docker_exec "yum install -y wget git"
 docker_exec "[ ! -f /$toolchain_tar_gz ] && wget -O /$toolchain_tar_gz $toolchain_url"
 docker_exec "[ ! -d /opt/toolchain-v4/ ] && tar -xzf /$toolchain_tar_gz -C /opt"
 docker_exec "[ ! -d /memgraph/ ] && git clone $memgraph_repo"
 # docker_exec "$setup_cmd"
 docker_exec "$mgconsole_build_cmd"
+mkdir -p "$PROJECT_ROOT/build/generic"
+docker cp -q "$docker_name:/mgconsole/build/src/mgconsole" "$PROJECT_ROOT/build/generic/"
