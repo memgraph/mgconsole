6e4598a688104e1a73778a634ebb4fab154e3777
matt <mattkjames7@gmail.com>
2026-02-06 17:40:59 +0000

    reorder test script args

--- tests/test-static.sh
+++ tests/test-static.sh
@@ -23,11 +23,10 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-client_binary=${1:-"$PROJECT_ROOT/build/generic/mgconsole"}
-memgraph_image=${2:-"memgraph/memgraph:latest"}
+memgraph_image=${1:-"memgraph/memgraph:latest"}
+client_binary=${2:-"$PROJECT_ROOT/build/generic/mgconsole"}
 
 echo "Testing mgconsole static build"
-
 script_args=()
 if $use_ssl; then
     script_args+=("--use-ssl")
@@ -36,7 +35,6 @@ if $use_docker; then
     script_args+=("--docker")
 fi
 script_args+=("$memgraph_image" "$client_binary")
-echo "Running tests with arguments: ${script_args[@]}"
 
 cd "$SCRIPT_DIR/input_output"
 ./run-tests.sh "${script_args[@]}"
